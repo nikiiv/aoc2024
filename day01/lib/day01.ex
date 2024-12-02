c74ee2ec3f0e4f936c58d7443e7cfa7474239928
Niki <niki@gmail.com>
2024-12-02 20:18:24 +0200

    First two days

---
+++ day01/lib/day01.ex
@@ -0,0 +1,73 @@
+defmodule Day01 do
+  @file_name "/Users/niki/aoc/day01/input.txt"
+  @file_name2 "/Users/niki/aoc/day01/small_input"
+
+  def parse_csv, do: parse_csv(@file_name)
+
+  def parse_csv(file_path) do
+    # Read the file content
+    {:ok, content} = File.read(file_path)
+
+    # Split the content into rows and process them
+    rows =
+      content
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parse_row/1)
+
+    # Separate the numbers into two lists
+    {list1, list2} = split_columns(rows)
+
+    IO.puts("First column: #{inspect(list1)}")
+    IO.puts("Second column: #{inspect(list2)}")
+
+    {list1, list2}
+  end
+
+  # Parse a single row into a tuple of numbers
+  defp parse_row(row) do
+    row
+    |> String.split("   ", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end
+
+  # Split rows into two separate lists for each column
+  defp split_columns(rows) do
+    Enum.reduce(rows, {[], []}, fn {a, b}, {list1, list2} ->
+      {[a | list1], [b | list2]}
+    end)
+    |> then(fn {list1, list2} -> {Enum.sort(list1), Enum.sort(list2)} end)
+  end
+
+  def count_occurrences(list, number) do
+    Enum.count(list, fn x -> x == number end)
+  end
+
+  def solve2 do
+    data = parse_csv(@file_name)
+    list1 = elem(data, 0)
+    list2 = elem(data, 1)
+    solve2(list1, list2, 0)
+  end
+
+  def solve2([h | t], list, acc) do
+    acc = acc + h * count_occurrences(list, h)
+    solve2(t, list, acc)
+  end
+
+  def solve2([], _, acc), do: acc
+
+  def solve do
+    data = parse_csv()
+    list1 = elem(data, 0)
+    list2 = elem(data, 1)
+    solve(list1, list2, 0)
+  end
+
+  def solve([], [], acc), do: acc
+
+  def solve([h1 | t1], [h2 | t2], acc) do
+    acc = acc + abs(h1 - h2)
+    solve(t1, t2, acc)
+  end
+end
---
+++ day02/lib/day02.ex
@@ -0,0 +1,102 @@
+defmodule Day02 do
+  @file_name "/Users/niki/aoc/day02/input.txt"
+  @file_name2 "/Users/niki/aoc/day02/small_input.txt"
+
+  def parse_file(file_path) do
+    # Read the file content
+    case File.read(file_path) do
+      {:ok, content} ->
+        content
+        |> String.split("\n", trim: true)
+        |> Enum.map(&parse_row/1)
+
+      {:error, reason} ->
+        IO.puts("Failed to read the file. Reason: #{inspect(reason)}")
+        {:error, reason}
+    end
+  end
+
+  # Parse a single row into a tuple of numbers
+  defp parse_row(row) do
+    row
+    |> String.split(" ", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def solve_example, do: solve_problem(@file_name2)
+
+  def solve_problem, do: solve_problem(@file_name)
+
+  def solve_problem(file_name) do
+    data = parse_file(file_name)
+    # IO.inspect(length(data))
+
+    # call solve for problem1, solver for problem 2
+    Enum.map(data, &solver/1)
+    |> Enum.count(fn x -> x == :safe end)
+  end
+
+  # def solve([], acc), do: acc
+
+  # def solve([h, t], acc) do
+  # end
+
+  def solver(orig_levels) do
+    IO.inspect(orig_levels)
+
+    variations =
+      Enum.map(0..(length(orig_levels) - 1), fn x -> List.delete_at(orig_levels, x) end)
+
+    IO.inspect(variations)
+    sol = Enum.any?(variations, fn l -> :safe == solve(l) end)
+    IO.inspect(sol)
+
+    if sol do
+      :safe
+    else
+      :unsafe
+    end
+  end
+
+  def solve(nums) do
+    direction = get_direction(nums)
+    # IO.inspect(direction)
+
+    case direction do
+      :desc ->
+        check(Enum.reverse(nums))
+
+      :asc ->
+        check(nums)
+    end
+  end
+
+  def check(nums) when length(nums) == 1, do: :safe
+
+  def check([h | t] = _l) do
+    # IO.puts("Checking #{inspect(l)} ")
+    el = List.first(t)
+    # IO.puts("Comparing #{inspect(h)} and #{inspect(el)}")
+
+    cond do
+      el == h -> :unsafe
+      el - h > 3 -> :unsafe
+      el - h < 4 and el - h > 0 -> check(t)
+      true -> :unsafe
+    end
+  end
+
+  def get_direction(l) do
+    nums = List.to_tuple(l)
+    size = tuple_size(nums)
+    # IO.puts("#{inspect(nums)} of size #{inspect(size)}")
+    last_elem = elem(nums, size - 1)
+    prev_elem = elem(nums, size - 2)
+    # IO.puts("L: #{inspect(last_elem)}, P: #{inspect(prev_elem)}")
+
+    cond do
+      last_elem > prev_elem -> :asc
+      last_elem <= prev_elem -> :desc
+    end
+  end
+end
