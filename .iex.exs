c44386da7c31fe45f359355de0227d2a5b6fd736
Niki <niki@gmail.com>
2024-12-05 15:59:03 +0200

    .iex change

--- .iex.exs
+++ .iex.exs
@@ -1,2 +1,18 @@
-IEx.configure(inspect: [charlists: :as_lists])
-
+IEx.configure(
+    colors: [
+      syntax_colors: [
+        number: :light_yellow,
+        atom: :light_cyan,
+        string: :light_black,
+        boolean: [:light_blue],
+        nil: [:magenta, :bright]
+      ],
+      ls_directory: :cyan,
+      ls_device: :yellow,
+      doc_code: :green,
+      doc_inline_code: :magenta,
+      doc_headings: [:cyan, :underline],
+      doc_title: [:cyan, :bright, :underline]
+    ],
+    inspect: [charlists: :as_lists]
+  )
